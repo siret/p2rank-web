b69bf7de6651b24f838d1a650d3398ae07ad5ab2
Petr Škoda <skodapetr@gmail.com>
2020-12-28 13:46:36 +0100

    Update prepare_database script

--- conservation/prepare_database.py
+++ conservation/prepare_database.py
@@ -4,12 +4,20 @@
 # Prepare BLAST database with default files.
 #
 
+import os
+import subprocess
 import blast_database
 
 
 def main():
     databases = blast_database.DATABASE_NAME_TO_URL.values()
-    blast_database.prepare_databases(databases)
+    blast_database.prepare_databases(execute_command, databases)
+
+
+def execute_command(command: str):
+    result = subprocess.run(command, shell=True, env=os.environ.copy())
+    # Throw for non-zero (failure) return code.
+    result.check_returncode()
 
 
 if __name__ == "__main__":
