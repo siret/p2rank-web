d8045026533521658f7318274a79f78200cb45f2
David Jakubec <david.jakubec@gmail.com>
2021-02-18 11:54:28 +0100

    Initial directory structure.

---
+++ conservation_hmm/conservation_hmm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+
+def conservation_hmm(FASTA_file, database_file, working_directory, target_file):
+    pass
+
+
+if __name__ == '__main__':
+    conservation_hmm(*argv[1:])
+
+
---
+++ conservation_hmm/tests/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATABASE_FILE="../database/uniprot_sprot.fasta"
+WORKING_DIRECTORY="./working/"
+
+for FASTA_FILE in *.fasta
+do
+	echo "Processing file:" ${FASTA_FILE}
+	../conservation_hmm.py ${FASTA_FILE} ${DATABASE_FILE} ${WORKING_DIRECTORY} ${FASTA_FILE}.ic
+done
+
+
