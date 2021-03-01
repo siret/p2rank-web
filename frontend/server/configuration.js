a544b83c678978ae7fe06ba35914f7481d69ed9b
Petr Škoda <skodapetr@gmail.com>
2021-03-01 18:41:35 +0100

    Update develop configuration

--- frontend/server/configuration.js
+++ frontend/server/configuration.js
@@ -4,9 +4,9 @@ module.exports = {
   "port": 8075,
   // Use this to server data from files. Thus you can develop
   // frontend without the need to run another component.
-  "task-runner-directory": "../data/database",
+  "task-runner-directory": "../../data/database",
   // Use the option bellow to proxy commands to task runner instance.
   // This allow you to run tasks or connect to existing instance (p2rank.cz)
   // of task runner.
-  "task-runner-service": "localhost:8020",
+  // "task-runner-service": "localhost:8020",
 };
